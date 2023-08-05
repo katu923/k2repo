6b17295abd2e79152184e1b87b54739dab53ff80
katu923 <141049626+katu923@users.noreply.github.com>
2023-08-05 12:54:26 +0100

    Update void_inst_efistub.bash

--- void_inst_efistub.bash
+++ void_inst_efistub.bash
@@ -30,7 +30,7 @@ graphical="kde" #empty "" it will install only base system and apps_minimal
 
 disk="/dev/sda" #or /dev/vda for virt-manager
 
-#secure_boot="yes" # better leave this empty "" you can break your bios
+secure_boot="yes" # better leave this empty "" you can break your bios
 
 void_repo="https://repo-fastly.voidlinux.org"
 #after install change mirror with xmirror
@@ -167,11 +167,11 @@ echo "fs.protected_regular=2" >> /mnt/usr/lib/sysctl.d/10-void.conf
 echo "fs.protected_fifos=2" >> /mnt/usr/lib/sysctl.d/10-void.conf
 echo "net.ipv4.conf.all.rp_filter=1" >> /mnt/etc/sysctl.conf
 
-#if [[ ! -z $secure_boot ]]; then
+if [[ ! -z $secure_boot ]]; then
 
-#chroot /mnt sbctl create-keys
-#chroot /mnt sbctl enroll-keys -m -i #this use microsoft keys to uefi secure boot
-#fi
+chroot /mnt sbctl create-keys
+chroot /mnt sbctl enroll-keys -m -i #this use microsoft keys to uefi secure boot
+fi
 
 
 # Options for the kernel hook script installed by the efibootmgr package.
@@ -189,7 +189,7 @@ echo "PART=1" >> /mnt/etc/default/efibootmgr-kernel-hook
 
 echo 'efibootmgr -qc $args -L "Void Linux with kernel ${major_version}" -l /efi/EFi/void/linux-${VERSION}.efi -u "${OPTIONS}"' >> /mnt/etc/kernel.d/post-install/50-efibootmgr
 
-#echo 'sbctl sign -s /boot/efi/EFI/void/linux-${VERSION}.efi' >> /mnt/etc/kernel.d/post-install/50-efibootmgr
+echo 'sbctl sign -s /boot/efi/EFI/void/linux-${VERSION}.efi' >> /mnt/etc/kernel.d/post-install/50-efibootmgr
 
 
 echo "CREATE_UEFI_BUNDLES=yes" >> /mnt/etc/default/dracut-uefi-hook
