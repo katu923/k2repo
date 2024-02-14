6de447e05d8f6a192f80cfc2cedaaa02eb9eadd1
katu923 <141049626+katu923@users.noreply.github.com>
2024-02-14 14:49:27 +0000

    Update gentooinstall.bash

--- gentooinstall.bash
+++ gentooinstall.bash
@@ -69,9 +69,15 @@ tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
 sed COMMON_FLAGS="-O2 -pipe"
 sed -i 's@"-02 -pipe"@"-march=native -O2 -pipe"@g' /mnt/gentoo/etc/portage/make.conf
 #arch-chroot /mnt/gentoo
+
+
 echo "MAKEOPTS="-j4 -l4" >> /mnt/gentoo/etc/portage/make.conf
 
 #INSTALL BASE SYSTEM
+cat << EOF | chroot /mnt/gentoo /bin/bash
+#<do anything as gentoo wiki explain or according your needs>
+ 
+
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
 
 mount --types proc /proc /mnt/gentoo/proc
@@ -85,34 +91,34 @@ mount --make-slave /mnt/gentoo/run
 
 
 
-$cr mkdir --parents /etc/portage/repos.conf
-$cr cp /usr/share/portage/config/repos.conf /etc/portage/repos.conf/gentoo.conf
+ mkdir --parents /etc/portage/repos.conf
+ cp /usr/share/portage/config/repos.conf /etc/portage/repos.conf/gentoo.conf
 
- $cr echo 'GENTOO_MIRRORS="https://mirrors.ptisp.pt/gentoo/"' >> /etc/portage/make.conf
+ echo 'GENTOO_MIRRORS="https://mirrors.ptisp.pt/gentoo/"' >> /etc/portage/make.conf
 
-$cr echo '[binhost]' > /etc/portage/binrepos.conf/gentoo.conf
-$cr echo 'priority = 9999' >> /etc/portage/binrepos.conf/gentoo.conf
-$cr echo 'sync-uri = https://mirrors.ptisp.pt/gentoo/releases/amd64/binpackages/17.1/x86-64/' >> /etc/portage/binrepos.conf/gentoo.conf
+ echo '[binhost]' > /etc/portage/binrepos.conf/gentoo.conf
+ echo 'priority = 9999' >> /etc/portage/binrepos.conf/gentoo.conf
+ echo 'sync-uri = https://mirrors.ptisp.pt/gentoo/releases/amd64/binpackages/17.1/x86-64/' >> /etc/portage/binrepos.conf/gentoo.conf
 
-$cr echo 'FEATURES="${FEATURES} getbinpkg"' >> /etc/portage/make.conf
-$cr echo 'FEATURES="${FEATURES} binpkg-request-signature"' >> /etc/portage/make.conf
+ echo 'FEATURES="${FEATURES} getbinpkg"' >> /etc/portage/make.conf
+ echo 'FEATURES="${FEATURES} binpkg-request-signature"' >> /etc/portage/make.conf
 
-$cr echo 'ACCEPT_LICENSE="-* @FREE @BINARY-REDISTRIBUTABLE"' >> /etc/portage/make.conf
+ echo 'ACCEPT_LICENSE="-* @FREE @BINARY-REDISTRIBUTABLE"' >> /etc/portage/make.conf
 
-$cr echo "Europe/Lisbon" > /etc/timezone
-$cr emerge --config sys-libs/timezone-data
-$cr echo "en_US ISO-8859-1" >> /etc/locale.gen
-$cr echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-$cr locale-gen
+ echo "Europe/Lisbon" > /etc/timezone
+ emerge --config sys-libs/timezone-data
+ echo "en_US ISO-8859-1" >> /etc/locale.gen
+ echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+ locale-gen
 
 #KERNEL CONFIG
 
-$cr emerge sys-kernel/linux-firmware
-$cr emerge sys-firmware/intel-microcode
-$cr echo "sys-kernel/installkernel dracut uki" >> /etc/portage/package.use/installkernel
-$cr emerge sys-kernel/gentoo-kernel-bin
-$cr echo 'uefi="yes"' >>  /etc/dracut.conf
-$cr echo 'kernel_cmdline="rd.luks.name='$luks_root_uuid'=cryptroot root=/dev/'$hostname'/root"' >> /etc/dracut.conf
+ emerge sys-kernel/linux-firmware
+ emerge sys-firmware/intel-microcode
+ echo "sys-kernel/installkernel dracut uki" >> /etc/portage/package.use/installkernel
+ emerge sys-kernel/gentoo-kernel-bin
+ echo 'uefi="yes"' >>  /etc/dracut.conf
+ echo 'kernel_cmdline="root=/dev/sda2 apparmor=1 security=apparmor"' >> /etc/dracut.conf
 
 #CONFIG SYSTEM
 
@@ -122,24 +128,24 @@ $cr echo 'kernel_cmdline="rd.luks.name='$luks_root_uuid'=cryptroot root=/dev/'$h
 
 
 
-$cr echo $hostname > /etc/hostname
+ echo $hostname > /etc/hostname
 
-$cr echo "$root_pw\n$root_pw" | passwd -q root
+ echo "$root_pw\n$root_pw" | passwd -q root
 
-$cr emerge sys-fs/xfsprogs
+ emerge sys-fs/xfsprogs
 
 #CONFIG BOOTLOADER
 
-$cr emerge sys-boot/efibootmgr
+ emerge sys-boot/efibootmgr
 
-$cr mkdir -p /efi/efi/gentoo
-$cr cp /boot/vmlinuz-* /efi/efi/gentoo/bzImage.efi 
-$cr efibootmgr --create --disk /dev/$disk --part 1 --label "gentoo_uefi" --loader "\efi\gentoo\bzImage.efi"
+ mkdir -p /efi/EFI/Linux
+  
+ efibootmgr --create --disk /dev/$disk --part 1 -l "\efi\gentoo\bzImage.efi"
 
 echo "target=home" >> /etc/conf.d/dmcrypt
 echo 'source="/dev/sda3"' >> /etc/conf.d/dmcrypt
 rc-update add dmcrypt boot
-
+EOF
 echo -e "\nUnmount gentoo installation and reboot?(y/n)\n"
 read tmp
 if [[ $tmp == "y" ]]; then
