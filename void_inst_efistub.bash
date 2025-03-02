cbae2df4703d035fa4d3a8b3e96b1adfdfc1fe30
katu923 <141049626+katu923@users.noreply.github.com>
2025-03-02 17:21:40 +0000

    Update void_inst_efistub.bash

--- void_inst_efistub.bash
+++ void_inst_efistub.bash
@@ -47,7 +47,7 @@ apps="xorg-minimal dejavu-fonts-ttf nano elogind dbus socklog-void apparmor chro
 apps_optional="rkhunter checksec lynis lm_sensors hplip firefox thunderbird bogofilter skype vlc ffmpeg"\
 " bash-completion libreoffice-calc libreoffice-writer libreoffice-impress hunspell-pt_PT" 
 
-apps_intel="mesa-dri xf86-video-intel libva-intel-driver intel-ucode intel-gpu-tools"
+apps_intel="mesa-dri libva-intel-driver intel-ucode intel-gpu-tools"
 
 apps_kde="kde5 kde5-baseapps kcron ark print-manager spectacle kdeconnect okular"\
 " plasma-wayland-protocols xdg-desktop-portal-kde plasma-applet-active-window-control skanlite gwenview"\
