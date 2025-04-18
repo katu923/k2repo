f6d7edc070c28edd90bf107af28b430a8bdbab6b
katu923 <141049626+katu923@users.noreply.github.com>
2025-04-18 13:57:37 +0100

    Update void_inst_efistub.bash

--- void_inst_efistub.bash
+++ void_inst_efistub.bash
@@ -239,7 +239,8 @@ xbps-install -SuyR $void_repo/current/$libc -r /mnt xbps
 #xbps-install -SyR $void_repo/current/$libc -r /mnt/ void-repo-nonfree
 
 if [[ ! -z $graphical ]]; then
-xbps-install -SyR $void_repo/current/$libc -r /mnt $apps ('apps_'$graphical) $apps_intel $apps_optional
+apps_graphical=$(echo 'apps_'$graphical)
+xbps-install -SyR $void_repo/current/$libc -r /mnt $apps apps_graphical $apps_intel $apps_optional
 #pipewire
 chroot /mnt mkdir -p /etc/pipewire/pipewire.conf.d
 chroot /mnt ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
